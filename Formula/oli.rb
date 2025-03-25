3dfcb8f44f6c83b95c83c026b14a28709b909578
Amrit Krishnan <amrit110@gmail.com>
2025-03-25 09:05:40 -0400

    oli 0.1.3
    Created by https://github.com/amrit110/oli/actions/runs/14060323285

--- Formula/oli.rb
+++ Formula/oli.rb
@@ -1,8 +1,8 @@
 class Oli < Formula
   desc "A simple, blazingly fast TUI based AI coding assistant"
   homepage "https://github.com/amrit110/oli"
-  url "https://github.com/amrit110/oli/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "7894bb73af701ef7e8b2cc6cd121a62d02ac83b8f1a57319dac6f745d23f5f12"
+  url "https://github.com/amrit110/oli/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "e51fa6ac248617f1b1c073c944f2a7d1107086d03ca319d9fda5c4fab76f76fa"
   license "Apache-2.0"
 
   depends_on "rust" => :build
